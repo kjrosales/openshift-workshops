0d89750bedd3c3fdea78de9fcc5dfe79bbdfedf3
Kyle Rosales <kjrosales72@gmail.com>
2019-09-11 11:29:14 -0500

    Test update

--- dc-metro-map/app.js
+++ dc-metro-map/app.js
@@ -7,12 +7,14 @@ var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
 
+//
 var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
+// Test edit
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
